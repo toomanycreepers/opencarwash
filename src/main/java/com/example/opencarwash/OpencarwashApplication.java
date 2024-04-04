2066401a540dcf40a9ef57323b1813f36da35142
Ya <toomanycreepers@mail.ru>
2024-04-04 18:18:45 +0500

    initial commit

---
+++ src/main/java/com/example/opencarwash/OpencarwashApplication.java
@@ -0,0 +1,13 @@
+package com.example.opencarwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencarwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpencarwashApplication.class, args);
+	}
+
+}
