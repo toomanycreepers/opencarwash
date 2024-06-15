c5fc654ab7230f2a7189b117fb081fb99afb1314
Ya <toomanycreepers@mail.ru>
2024-06-15 18:03:00 +0500

    Built app using gradle, on average launches slightly faster.

---
+++ src/main/java/com/example/opencarwash/dtos/order/OrderStartEndTimeDTO.java
@@ -0,0 +1,10 @@
+package com.example.opencarwash.dtos.order;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class OrderStartEndTimeDTO {
+    public String orderId;
+    public String startTime;
+    public String endTime;
+}
